f5fc71fca7cc1040028dfb3c17e06acab4430db8
SeoMinJong <65899572+SeoMinJong@users.noreply.github.com>
2024-03-15 20:56:58 +0900

    generic type

---
+++ 10_generic/3_generic_in_type.ts
@@ -0,0 +1,36 @@
+/**
+ * Generic in Type
+ */
+
+type GenericSimpleType<T> = T;
+
+const genericString: GenericSimpleType<string> = 'string';
+// const genericString2: GenericSimpleType = 'string';
+
+interface DoneState<T>{
+    data: T[];
+}
+
+interface LoadingState{
+    requestedAt: Date;
+}
+
+interface ErrorState{
+    error: string;
+}
+
+type State<T = number> = DoneState<T> | LoadingState | ErrorState;
+
+let state:  State<string> = {
+    data: ['123', '456'],
+}
+
+state = {
+    requestedAt: new Date()
+}
+
+state = {error: 'error'}
+
+let state2: State = {
+    data: [123, 456]
+}
