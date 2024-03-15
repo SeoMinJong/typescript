1ad4aaf6f9c49cf666534d9c94bf30879d2c7c1d
SeoMinJong <65899572+SeoMinJong@users.noreply.github.com>
2024-03-15 20:32:52 +0900

    generic intarface & generic defaultValue

---
+++ 10_generic/2_generic_in_interface.ts
@@ -0,0 +1,25 @@
+/**
+ * Generic In Interface
+ */
+interface Cache<T>{
+    data: T[];
+    lastUpdate: Date;
+}
+
+const cache1: Cache<string> = {
+    data: ['data1','data2'],
+    lastUpdate: new Date(),
+}
+
+const cache2: Cache<number> = {
+    data: [123, 1352],
+    lastUpdate: new Date(),
+}
+
+interface DefaultGeneric<T = string>{
+    data:T[];
+}
+
+const cache3:DefaultGeneric = {
+    data: ['stin', 'sfas'],
+}
